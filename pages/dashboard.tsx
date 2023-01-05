ca3802f54acb5ccaf351344590884e7347df18cc
Jason Gong <jasongong83@gmail.com>
2023-01-05 10:52:58 -0500

    add login page and initial dashboard

---
+++ pages/dashboard.tsx
@@ -0,0 +1,19 @@
+import { GetServerSidePropsContext, NextPage } from "next";
+import { unstable_getServerSession } from "next-auth/next";
+import { Session } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]";
+
+const Hosts: NextPage<{ session: Session }> = ({ session }) => {
+  return (
+    <div>
+      {session && (
+        <div className="mx-auto">
+          Session!!!
+        </div>
+      )}
+      {!session && <div>Missing a session</div>}
+    </div>
+  );
+};
+
+export default Hosts;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,68 @@
-export default function Home() {
-  return <h1 className="text-3xl font-bold underline text-red-400">Hello world!</h1>;
-}
+import { signIn } from "next-auth/react";
+import Head from "next/head";
+import { FormEvent, ReactElement } from "react";
+import { NextPageWithLayout } from "./_app";
+
+const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+  event.preventDefault();
+
+  const email = (
+    event.currentTarget.elements.namedItem("email") as HTMLInputElement
+  ).value;
+  const password = (
+    event.currentTarget.elements.namedItem("password") as HTMLInputElement
+  ).value;
+
+  signIn("credentials", { email, password, callbackUrl: "/dashboard" });
+};
+
+const Home: NextPageWithLayout = () => {
+  return (
+    <div>
+      <Head>
+        <title>HCM Show</title>
+        <meta name="description" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-row items-center justify-center min-h-screen">
+        <div>
+          <h1 className="mb-4">Log in to your account</h1>
+          <div>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-2">
+                <label className="mr-2">Email</label>
+                <input
+                  name="email"
+                  type="email"
+                  placeholder="Enter your email"
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label className="mr-2">Password</label>
+                <input
+                  name="password"
+                  type="password"
+                  placeholder="Enter your password"
+                  required
+                />
+              </div>
+              <button className="px-6 py-1 mt-4 bg-white text-black rounded-lg">
+                Log in
+              </button>
+            </form>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+Home.getLayout = function getLayout(page: ReactElement) {
+  return page;
+};
+
+Home.skipAuth = true;
+
+export default Home;
