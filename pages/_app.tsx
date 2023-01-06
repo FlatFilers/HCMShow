31235dcc8547ff6e6d948b43b0bb267d588f4a72
John Mosesman <johnmosesman@gmail.com>
2023-01-06 16:22:19 -0600

    Adds layout component and wires up the routes we have

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import type { AppProps } from "next/app";
 import { SessionProvider, useSession } from "next-auth/react";
 import { Session } from "next-auth";
 import { useRouter } from "next/router";
+import SidebarLayout from "../components/sidebar-layout";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -20,7 +21,8 @@ export default function MyApp(props: AppPropsWithLayout) {
   const { Component, session, pageProps } = props;
 
   // Use the layout defined at the page level, if available
-  const getLayout = Component.getLayout ?? ((page) => page);
+  const getLayout =
+    Component.getLayout ?? ((page) => <SidebarLayout>{page}</SidebarLayout>);
   const skipAuth = Component.skipAuth;
 
   return getLayout(
