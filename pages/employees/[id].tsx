a9a50c35bbde237b784d0a568fda6ff1b02cd72e
John Mosesman <johnmosesman@gmail.com>
2023-01-06 15:27:32 -0600

    WIP layout for employees + /id

--- pages/employees/[id].tsx
+++ pages/employees/[id].tsx
@@ -7,18 +7,43 @@ interface Props {
 
 const Employees: NextPage<Props> = ({ employee }) => {
   return (
-    <div>
-      {!employee && (
-        <div>
-          <p>No employee found</p>
-        </div>
-      )}
-
-      {employee && (
-        <div>
-          <p>ID: {employee.id}</p>
-        </div>
-      )}
+    <div className="overflow-hidden bg-white shadow sm:rounded-lg">
+      <div className="px-4 py-5 sm:px-6">
+        <h3 className="text-lg font-medium leading-6 text-gray-900">
+          Employee Information
+        </h3>
+        <p className="mt-1 max-w-2xl text-sm text-gray-500">
+          Employee personal details
+        </p>
+      </div>
+      <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
+        <dl className="sm:divide-y sm:divide-gray-200">
+          <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Full name</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              {employee.id} - Margot Foster
+            </dd>
+          </div>
+          <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Manager</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              {employee.managerId}
+            </dd>
+          </div>
+          <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Title</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              {employee.positionTitle}
+            </dd>
+          </div>
+          <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Location</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              {employee.locationId}
+            </dd>
+          </div>
+        </dl>
+      </div>
     </div>
   );
 };
