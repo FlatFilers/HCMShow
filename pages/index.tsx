904205b67595460c5e5c708d35228fe47acbfddc
mhpjay422 <12041898+mhpjay422@users.noreply.github.com>
2023-01-06 17:25:54 -0500

    fix conflict (#7)

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { signIn } from "next-auth/react";
 import Head from "next/head";
 import { FormEvent, ReactElement } from "react";
 import { NextPageWithLayout } from "./_app";
+import { LockClosedIcon } from "@heroicons/react/20/solid";
 
 const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
   event.preventDefault();
@@ -18,43 +19,102 @@ const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
 
 const Home: NextPageWithLayout = () => {
   return (
-    <div className="">
-      <Head>
-        <title>HCM Show</title>
-        <meta name="description" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-row items-center justify-center min-h-screen bg-white">
+    <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-white h-screen">
+      <div className="w-full max-w-md space-y-8">
         <div>
-          <h1 className="mb-4 text-black">Log in to your account</h1>
-          <div>
-            <form onSubmit={handleSubmit}>
-              <div className="mb-2">
-                <label className="mr-2 text-black">Email</label>
-                <input
-                  name="email"
-                  type="email"
-                  placeholder="Enter your email"
-                  required
-                />
-              </div>
-              <div className="mb-4">
-                <label className="mr-2 text-black">Password</label>
-                <input
-                  name="password"
-                  type="password"
-                  placeholder="Enter your password"
-                  required
-                />
-              </div>
-              <button className="px-6 py-1 mt-4 bg-white text-black rounded-lg border border-black">
-                Log in
-              </button>
-            </form>
-          </div>
+          <img
+            className="mx-auto h-12 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+          <p className="mt-2 text-center text-sm text-gray-600">
+            Or{" "}
+            <a
+              href="#"
+              className="font-medium text-indigo-600 hover:text-indigo-500"
+            >
+              start your 14-day free trial
+            </a>
+          </p>
         </div>
-      </main>
+        <form className="mt-8 space-y-6" action="#" onSubmit={handleSubmit}>
+          <input type="hidden" name="remember" defaultValue="true" />
+          <div className="-space-y-px rounded-md shadow-sm">
+            <div>
+              <label htmlFor="email-address" className="sr-only">
+                Email address
+              </label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="bg-white relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                placeholder="Email address"
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="bg-white relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                placeholder="Password"
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <input
+                id="remember-me"
+                name="remember-me"
+                type="checkbox"
+                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+              />
+              <label
+                htmlFor="remember-me"
+                className="ml-2 block text-sm text-gray-900"
+              >
+                Remember me
+              </label>
+            </div>
+
+            <div className="text-sm">
+              <a
+                href="#"
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                Forgot your password?
+              </a>
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                <LockClosedIcon
+                  className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                  aria-hidden="true"
+                />
+              </span>
+              Sign in
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
