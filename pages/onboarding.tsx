ed6f78e8c313ff9addc844628ab0caa225fadef2
John Mosesman <johnmosesman@gmail.com>
2023-01-17 09:21:50 -0600

    Page style tweak

--- pages/onboarding.tsx
+++ pages/onboarding.tsx
@@ -10,20 +10,29 @@ const Onboarding: NextPage<Props> = () => {
 
   return (
     <div className="px-4 text-gray-900">
-      <p>Onboarding Flow</p>
+      <p className="text-2xl mb-8">Onboarding Setup</p>
+
+      <p className="mb-2">First, download the sample data below. 👇</p>
 
       <a
-        className="underline"
+        className="hover:text-white mb-12 inline-flex items-center justify-center rounded-md border text-gray-900 border-indigo-600 px-4 py-2 text-sm font-medium shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
         download={"sample-employees.csv"}
         href={"/sample-data/sample-employees.csv"}
       >
-        Click here to download the sample data
+        Download sample data
       </a>
 
-      <p>TODO: Tenant ID</p>
+      <p className="mb-2">
+        Next click the button below to begin your onboarding in Flatfile. 👇
+      </p>
 
       <form action="/api/flatfile/create-space">
-        <button type="submit">Submit</button>
+        <button
+          className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+          type="submit"
+        >
+          Create Space
+        </button>
       </form>
     </div>
   );
