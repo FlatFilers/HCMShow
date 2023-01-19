cf7204bd75f49b19c5fb921e178fca3969868fb3
Jason Gong <jasongong83@gmail.com>
2023-01-19 14:42:01 -0500

    remove config and set conditionals in authorize.

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,7 @@ import withAuth from "next-auth/middleware";
 import { NextResponse } from "next/server";
 import type { NextRequest } from 'next/server'
 
-// config determines which paths are authed by the [...nextauth].ts file.] 
-
-// Then the authorized callback, when returning true, will hit the middleware. Returning false will redirect to the signin page.
+// The authorized callback, when returning true, will hit the middleware. Returning false will redirect to the signin page.
 
 export default withAuth(
   function middleware(req) {
@@ -13,7 +11,9 @@ export default withAuth(
   {
     callbacks: {
       authorized: ({ req,token }) => {
-        if (req.nextUrl.pathname === "/") {
+        if (req.nextUrl.pathname === "/" ||
+            req.nextUrl.pathname.includes("api/auth")
+        ) {
 
           return true;
         }
@@ -24,11 +24,3 @@ export default withAuth(
   }
 );
 
-// matcher is a list of authed paths. 
-
-// Added regex below that reverses that and says to auth all routes except these
-// To add a route, add a standpipe to the end of the last path and add the new path
-// ie ["/((?!auth/signin).*)"] -> ["/((?!auth/signin|NEW_ROUTE).*)"] 
-export const config = {
-  matcher: ["/((?!auth/signin).*)"],
-};
