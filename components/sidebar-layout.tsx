eb2c343027d6e11109d8e33147fe68c7039b79fa
Jason Gong <jasongong83@gmail.com>
2023-01-24 17:57:14 -0500

    edit changes to non-desktop as

--- components/sidebar-layout.tsx
+++ components/sidebar-layout.tsx
@@ -121,7 +121,7 @@ const SidebarLayout = ({ children }: Props) => {
                         href={item.href}
                         className={classNames(
                           item.current
-                            ? "bg-gray-100 text-gray-900"
+                            ? "bg-slate-200 text-gray-900"
                             : "text-gray-600 hover:bg-gray-50 hover:text-gray-900",
                           "group flex items-center px-2 py-2 text-base font-medium rounded-md"
                         )}
@@ -129,7 +129,7 @@ const SidebarLayout = ({ children }: Props) => {
                         <item.icon
                           className={classNames(
                             item.current
-                              ? "text-gray-500"
+                              ? "text-gray-800"
                               : "text-gray-400 group-hover:text-gray-500",
                             "mr-4 flex-shrink-0 h-6 w-6"
                           )}
