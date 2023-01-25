e28a24019babec933fe67e9f3ce2e72418ea0c18
John Mosesman <johnmosesman@users.noreply.github.com>
2023-01-25 13:37:44 -0600

    Mobile sign out style (#41)

--- components/sidebar-layout.tsx
+++ components/sidebar-layout.tsx
@@ -163,64 +163,25 @@ const SidebarLayout = ({ children }: Props) => {
                         Imports
                       </a>
                       {/* Profile dropdown */}
-                      <div className="flex flex-row w-full border-t-2 border-gray-200">
-                        <Menu
-                          as="div"
-                          className="relative ml-auto group flex items-center px-2 pt-4 text-base font-medium rounded-md"
+                      <div className="flex flex-row w-full border-t-2 border-gray-200 pt-1">
+                        <a
+                          key="Imports"
+                          href="#"
+                          onClick={() => signOut()}
+                          className={classNames(
+                            "text-gray-600 hover:bg-gray-50 hover:text-gray-900",
+                            "w-full group flex items-center px-2 py-2 text-sm font-medium rounded-md mb-1"
+                          )}
                         >
-                          <div>
-                            <Menu.Button className="flex max-w-xs items-center rounded-full bg-gray-100 text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                              <span className="sr-only">Open user menu</span>
-                              <img
-                                className="h-8 w-8 rounded-full"
-                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                alt=""
-                              />
-                            </Menu.Button>
-                          </div>
-                          <Transition
-                            as={Fragment}
-                            enter="transition ease-out duration-100"
-                            enterFrom="transform opacity-0 scale-95"
-                            enterTo="transform opacity-100 scale-100"
-                            leave="transition ease-in duration-75"
-                            leaveFrom="transform opacity-100 scale-100"
-                            leaveTo="transform opacity-0 scale-95"
-                          >
-                            <Menu.Items className="absolute right-0 bottom-10 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                              {userNavigation.map((item) => (
-                                <Menu.Item key={item.name}>
-                                  {({ active }) => (
-                                    <a
-                                      href={item.href}
-                                      className={classNames(
-                                        active ? "bg-gray-100" : "",
-                                        "block px-4 py-2 text-sm text-gray-700"
-                                      )}
-                                    >
-                                      {item.name}
-                                    </a>
-                                  )}
-                                </Menu.Item>
-                              ))}
-
-                              <Menu.Item>
-                                {({ active }) => (
-                                  <a
-                                    href="#"
-                                    onClick={() => signOut()}
-                                    className={classNames(
-                                      active ? "bg-gray-100" : "",
-                                      "block px-4 py-2 text-sm text-gray-700"
-                                    )}
-                                  >
-                                    Sign out
-                                  </a>
-                                )}
-                              </Menu.Item>
-                            </Menu.Items>
-                          </Transition>
-                        </Menu>
+                          <ArrowLeftOnRectangleIcon
+                            className={classNames(
+                              "text-gray-400 group-hover:text-gray-500",
+                              "mr-4 flex-shrink-0 h-6 w-6"
+                            )}
+                            aria-hidden="true"
+                          />
+                          Sign Out
+                        </a>
                       </div>
                     </div>
                   </nav>
