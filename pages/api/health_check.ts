fbf05b133c0fb198bcf54a795413d2d2f139e11e
Jason Gong <jasongong83@gmail.com>
2023-01-31 14:10:06 -0500

    edit import to lib prisma client

--- pages/api/health_check.ts
+++ pages/api/health_check.ts
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { PrismaClient } from '@prisma/client';
+import { prismaClient } from "../../lib/prisma-client";
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
@@ -10,9 +10,8 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const prisma = new PrismaClient();
-  
-  await prisma.$queryRaw`SELECT 1;`;
+
+  await prismaClient.$queryRaw`SELECT 1;`;
 
   res.status(200).json({
     status: 'Healthy',
