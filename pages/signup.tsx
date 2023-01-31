b6e3ed6866186d34c1433c324e83c1ea6add5a56
Jason Gong <jasongong83@gmail.com>
2023-01-31 04:46:36 -0500

    update styling

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -34,20 +34,17 @@ const Signup: NextPageWithLayout = () => {
   return (
     <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-white h-screen">
       <div className="w-full max-w-md space-y-8">
-      <div>
-        <img
-          className="mx-auto h-12 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt="Your Company"
-        />
-        <h2 className="mt-6 text-left text-3xl font-bold tracking-tight text-gray-900">
-          Create your account below to
-        </h2>
-        <div className="mt-6 text-3xl font-bold tracking-tight text-gray-900">
-        Sign up for HCM.show
+        <div>
+          <img
+            className="mx-auto h-12 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+          <div className="text-gray-900">Create your account below to </div>
+          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+          Sign up for HCM.show
+          </h2>
         </div>
-      </div>
-        
         <form className="mt-8 space-y-2" action="#" onSubmit={handleSubmit}>
           {error && (
             <div
