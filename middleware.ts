22297bc466a06503b1d2a31d26714c6777d4be6a
mhpjay422 <12041898+mhpjay422@users.noreply.github.com>
2023-02-03 13:26:28 -0500

    fix route for auth (#63)

--- middleware.ts
+++ middleware.ts
@@ -21,7 +21,7 @@ export default withAuth(
         if (
           req.nextUrl.pathname === "/" ||
           req.nextUrl.pathname === "/signup" ||
-          req.nextUrl.pathname === "api/health_check" ||
+          req.nextUrl.pathname === "/api/health_check" ||
           req.nextUrl.pathname === "/api/seeds" ||
           req.nextUrl.pathname.includes("api/v1") ||
           req.nextUrl.pathname.includes("api/auth")
