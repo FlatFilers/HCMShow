fc07c202ea9fedf944b3e6fa26a209a2c8c23f9a
mhpjay422 <12041898+mhpjay422@users.noreply.github.com>
2023-02-08 21:55:01 -0500

    Edit logo and change UI to match. (#65)
    * fix conflict
    
    * fix conflict
    
    * fix conflict
    
    * set default grayscale on hovered primary buttons.
    
    * fix conflict
    
    * fix conflict
    
    * fix conflict
    
    * fix conflict
    
    * fix conflict
    
    * fix conflict
    
    * add primary dark color
    
    * edit text and replace grayscale
    
    * fix conflict
    
    * declaring modules
    
    * fix conflict
    
    * fix conflict
    
    * Change focus color to primary
    
    ---------
    
    Co-authored-by: John Mosesman <johnmosesman@gmail.com>

--- middleware.ts
+++ middleware.ts
@@ -23,6 +23,7 @@ export default withAuth(
           req.nextUrl.pathname === "/signup" ||
           req.nextUrl.pathname === "/api/health_check" ||
           req.nextUrl.pathname === "/api/seeds" ||
+          req.nextUrl.pathname.includes("/images") ||
           req.nextUrl.pathname.includes("api/v1") ||
           req.nextUrl.pathname.includes("api/auth")
         ) {
