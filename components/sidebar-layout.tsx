b9216380cfb970e4a7d192a436412bf32a75a03b
mhpjay422 <12041898+mhpjay422@users.noreply.github.com>
2023-02-16 16:28:49 -0500

    Edit job index and detail (#76)
    * edit job index and detail
    
    * import type and edit show view
    
    * Couple tweaks
    
    ---------
    
    Co-authored-by: John Mosesman <johnmosesman@gmail.com>

--- components/sidebar-layout.tsx
+++ components/sidebar-layout.tsx
@@ -77,7 +77,7 @@ const SidebarLayout = ({ children }: Props) => {
   }
 
   return (
-    <div className="h-screen w-screen bg-white">
+    <div className="h-screen w-screen bg-white overflow-hidden">
       <Toaster />
 
       <MobileSidebar
--- pages/jobs.tsx
+++ pages/jobs.tsx
@@ -1,23 +1,16 @@
 import { GetServerSideProps, NextPage } from "next";
-import { Prisma, PrismaClient } from "@prisma/client";
+import { Job, Prisma, PrismaClient } from "@prisma/client";
 import Link from "next/link";
 import { getToken } from "next-auth/jwt";
-
-type JobFamily = {
-  id: string;
-  name: string;
-  effectiveDate: Date | null;
-  summary: string | null;
-  isInactive: boolean;
-};
+import { DateTime } from "luxon";
 
 interface Props {
-  jobs: JobFamily[];
+  jobs: Job[];
 }
 
 const Jobs: NextPage<Props> = ({ jobs }) => {
   return (
-    <div className="px-4 sm:px-6 lg:px-8">
+    <div className="px-4 sm:px-6 lg:px-8 h-screen w-screen">
       <div className="sm:flex sm:items-center">
         <div className="sm:flex-auto">
           <h1 className="text-xl font-semibold text-gray-900">Jobs</h1>
@@ -27,70 +20,98 @@ const Jobs: NextPage<Props> = ({ jobs }) => {
           </p>
         </div>
       </div>
-      <div className="mt-8 flex flex-col">
-        <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-              <table className="min-w-full divide-y divide-gray-300">
-                <thead className="bg-gray-50">
-                  <tr>
-                    <th
-                      scope="col"
-                      className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
-                    >
-                      Name
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                    >
-                      Effective Date
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                    >
-                      Summary
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                    >
-                      Status
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="divide-y divide-gray-200 bg-white">
-                  {jobs.map((job) => (
-                    <tr key={job.id}>
-                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
-                        <Link href={`/jobs/${job.id}`}>{job.name}</Link>
-                      </td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                        {<div>{job.effectiveDate?.toString()}</div>}
-                      </td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                        {job.summary}
-                      </td>
-
-                      <td
-                        className={`relative whitespace-nowrap py-4 pl-3 pr-4 text-sm font-medium sm:pr-6
-                           ${
-                             job.isInactive ? "text-red-500" : "text-green-500"
-                           }`}
-                      >
-                        {job.isInactive ? (
-                          <div>Inactive</div>
-                        ) : (
-                          <div>Active</div>
+      <div className="my-8 w-10/12 h-5/6 overflow-auto">
+        <div className="border border-gray-100 rounded-l-lg">
+          <table className="h-full w-full">
+            <thead className="bg-gray-50 sticky inset-0 w-full h-20">
+              <tr>
+                <th
+                  scope="col"
+                  className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                >
+                  Name
+                </th>
+                <th
+                  scope="col"
+                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                >
+                  Effective Date
+                </th>
+                <th
+                  scope="col"
+                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                >
+                  Summary
+                </th>
+                <th
+                  scope="col"
+                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                >
+                  Description
+                </th>
+                <th
+                  scope="col"
+                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                >
+                  Work Shift Required
+                </th>
+                <th
+                  scope="col"
+                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                >
+                  Public Job
+                </th>
+                <th
+                  scope="col"
+                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                >
+                  Status
+                </th>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-200 bg-white w-full">
+              {jobs.map((job) => (
+                <tr key={job.id}>
+                  <td className="text-blue-600 underline whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium sm:pl-6">
+                    <Link href={`/jobs/${job.id}`}>{job.name}</Link>
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                    {
+                      <div>
+                        {DateTime.fromJSDate(job.effectiveDate).toFormat(
+                          "MM/dd/yyyy"
                         )}
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
+                      </div>
+                    }
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                    {job.summary ? job.summary?.slice(0, 40) + "..." : ""}
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                    {job.description
+                      ? job.description?.slice(0, 40) + "..."
+                      : ""}
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                    {job.workShift ? "Yes" : "No"}
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                    {job.jobPublic ? "Yes" : "No"}
+                  </td>
+                  <td
+                    className={`whitespace-nowrap py-4 pl-3 pr-4 text-sm font-medium sm:pr-6
+                             ${
+                               job.isInactive
+                                 ? "text-red-500"
+                                 : "text-green-500"
+                             }`}
+                  >
+                    {job.isInactive ? <div>Inactive</div> : <div>Active</div>}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
@@ -112,7 +133,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
   const prisma = new PrismaClient();
 
-  const jobs: JobFamily[] = await prisma.jobFamily.findMany();
+  const jobs: Job[] = await prisma.job.findMany();
 
   return {
     props: {
