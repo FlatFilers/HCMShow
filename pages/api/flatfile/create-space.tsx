9d9467ee5cc92582dde6db911a1455cc2ee7f909
Jason Gong <jasongong83@gmail.com>
2023-02-22 21:01:07 -0500

    hardcode space type. remove new migration and edit space migration

--- pages/api/flatfile/create-space.tsx
+++ pages/api/flatfile/create-space.tsx
@@ -2,7 +2,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Prisma, PrismaClient, Space } from "@prisma/client";
 import { getToken } from "next-auth/jwt";
-import { useRouter } from "next/router";
 import {
   addDocumentToSpace,
   addGuestToSpace,
@@ -16,7 +15,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const router = useRouter();
   const token = await getToken({
     req: req,
   });
@@ -79,7 +77,7 @@ export default async function handler(
       userId: user.id,
       flatfileData:
         flatfileSpaceDataRefetch as unknown as Prisma.InputJsonValue,
-      type: router.query.spaceType as string,
+      type: "workbookUpload",
     },
   });
 
