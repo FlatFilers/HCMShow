27c210c9a03b9cc702b46833a5273c986f313488
Jason Gong <jasongong83@gmail.com>
2023-02-27 13:28:57 -0500

    add nullable column and backfill

--- prisma/migrations/20230224223053_add_org_to_jobs/migration.sql
+++ prisma/migrations/20230224223053_add_org_to_jobs/migration.sql
@@ -5,7 +5,9 @@
 
 */
 -- AlterTable
-ALTER TABLE "Job" ADD COLUMN     "organizationId" UUID NOT NULL;
+ALTER TABLE "Job" ADD COLUMN     "organizationId" UUID NOT NULL DEFAULT uuid_generate_v4();
+
+ALTER TABLE "Job" ALTER COLUMN "organizationId" DROP DEFAULT;
 
 -- AddForeignKey
 ALTER TABLE "Job" ADD CONSTRAINT "Job_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "Organization"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
