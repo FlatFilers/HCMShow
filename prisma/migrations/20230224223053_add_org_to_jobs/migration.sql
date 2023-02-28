35c948b767cd3d97682d66722510727322376102
Jason Gong <jasongong83@gmail.com>
2023-02-28 17:54:32 -0500

    fix migration. remove update.

--- prisma/migrations/20230224223053_add_org_to_jobs/migration.sql
+++ prisma/migrations/20230224223053_add_org_to_jobs/migration.sql
@@ -5,15 +5,7 @@
 
 */
 -- AlterTable
-ALTER TABLE "Job" ADD COLUMN     "organizationId" UUID NULL;
-
-UPDATE "Job" 
-SET "organizationId" = "Employee"."organizationId"
-FROM "Employee"
-JOIN "EmployeesJobs" ON "EmployeesJobs"."employeeId" = "Employee"."id"
-WHERE "Job"."id" = "EmployeesJobs"."jobId";
-
-ALTER TABLE "Job" ALTER COLUMN "organizationId" SET NOT NULL;
+ALTER TABLE "Job" ADD COLUMN     "organizationId" UUID SET NOT NULL;
 
 -- AddForeignKey
 ALTER TABLE "Job" ADD CONSTRAINT "Job_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "Organization"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
