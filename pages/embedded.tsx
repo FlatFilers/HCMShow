26a5161ef9b6c0aa63d33fd5547f2e795b72b303
Jason Gong <jasongong83@gmail.com>
2023-03-07 14:07:55 -0500

    add comment

--- pages/embedded.tsx
+++ pages/embedded.tsx
@@ -127,6 +127,7 @@ const Embedded: NextPageWithLayout<Props> = ({
               </div>
             </div>
             <div>
+              {/* TODO: Add creating space toast and toast on redirect */}
               <form action="/api/flatfile/create-embed-space">
                 <button className="px-4 py-2 inline-flex items-center justify-center rounded-md border text-sm font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 sm:w-auto mb-6 bg-primary text-white border-transparent">
                   Create Space
