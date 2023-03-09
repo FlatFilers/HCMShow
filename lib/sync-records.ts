655ff9fc33d9b3b60cdbfdce46e6b3b219fcfd14
John Mosesman <johnmosesman@users.noreply.github.com>
2023-03-08 20:42:59 -0600

    Cut fields to match config updates (#100)
    * Cut employee fields
    
    * Fix sync

--- lib/sync-records.ts
+++ lib/sync-records.ts
@@ -1,13 +1,4 @@
-import {
-  AdditionalJobClassification,
-  EmployeeType,
-  HireReason,
-  PayRate,
-  PositionTime,
-  Title,
-  WorkerCompensationCode,
-  Location,
-} from "@prisma/client";
+import { EmployeeType } from "@prisma/client";
 import { upsertEmployee, validEmployeeRecords } from "./employee";
 import { getAccessToken, getRecordsByName } from "./flatfile";
 import { prismaClient } from "./prisma-client";
@@ -56,16 +47,6 @@ export const syncWorkbookRecords = async ({
     return a.values.managerId.value ? 1 : -1;
   });
 
-  // TODO - hacking this in to get seeds working then do this
-  const workerCompensationCodeId = (
-    (await prismaClient.workerCompensationCode.findFirst(
-      {}
-    )) as WorkerCompensationCode
-  ).id;
-  const addresses = await prismaClient.address.findMany({
-    take: 2,
-  });
-
   const upsertEmployees = validsManagersFirst.map(async (r) => {
     try {
       const values = r.values;
@@ -75,156 +56,27 @@ export const syncWorkbookRecords = async ({
         })) as EmployeeType
       ).id;
 
-      // TODO: Needs SDK validation to map to ID
-      // const hireReasonId = (
-      //   (await prismaClient.hireReason.findUnique({
-      //     where: {
-      //       subcategorySlug: r.values.hireReason.value as string,
-      //     },
-      //   })) as HireReason
-      // ).id;
-      const hireReasonId = (
-        (await prismaClient.hireReason.findFirst()) as HireReason
-      ).id;
-
-      // TODO: Missing locations in base data
-      let locationId;
-      try {
-        locationId = (
-          (await prismaClient.location.findUnique({
-            where: { slug: r.values.location.value as string },
-          })) as Location
-        ).id;
-      } catch (error) {
-        console.error(
-          "Error - record ID:",
-          r.id,
-          ", location slug not found:",
-          r.values.location.value
-        );
-
-        locationId = ((await prismaClient.location.findFirst({})) as Location)
-          .id;
-      }
-
-      const positionTimeId = (
-        (await prismaClient.positionTime.findUnique({
-          where: { slug: r.values.positionTimeType.value as string },
-        })) as PositionTime
-      ).id;
-
-      // TODO: map fields in SDK
-      let payRateId;
-      try {
-        payRateId = (
-          (await prismaClient.payRate.findUnique({
-            where: { slug: r.values.payRateType.value as string },
-          })) as PayRate
-        ).id;
-      } catch (error) {
-        console.error(
-          "Error - record ID:",
-          r.id,
-          ", pay rate not found:",
-          r.values.payRateType.value
-        );
-
-        payRateId = ((await prismaClient.payRate.findFirst()) as PayRate).id;
-      }
-
       let data: Parameters<typeof upsertEmployee>[0] = {
         organizationId,
         employeeId: r.values.employeeId.value as string,
-        hireReasonId,
         firstName: r.values.firstName.value as string,
         lastName: r.values.lastName.value as string,
         hireDate: DateTime.fromFormat(
           r.values.hireDate.value as string,
           "yyyy-MM-dd"
         ).toJSDate(),
-        endEmploymentDate: r.values.hireDate.value
+        endEmploymentDate: r.values.endEmploymentDate.value
           ? DateTime.fromFormat(
-              r.values.hireDate.value as string,
+              r.values.endEmploymentDate.value as string,
               "yyyy-MM-dd"
             ).toJSDate()
           : null,
         positionTitle: r.values.positionTitle.value as string,
-        businessTitle: r.values.businessTitle.value as string,
-        locationId,
         employeeTypeId,
-        positionTimeId,
         defaultWeeklyHours: r.values.defaultWeeklyHours.value as number,
         scheduledWeeklyHours: r.values.scheduledWeeklyHours.value as number,
-        payRateId,
-        workerCompensationCodeId,
-        addresses,
         flatfileRecordId: r.id,
       };
-
-      if (values.middleName.value) {
-        data = { ...data, middleName: values.middleName.value as string };
-      }
-
-      if (values.title.value) {
-        const titleId = (
-          (await prismaClient.title.findUnique({
-            where: {
-              slug: values.title.value as string,
-            },
-          })) as Title
-        ).id;
-
-        data = { ...data, titleId };
-      }
-
-      if (values.socialSuffix.value) {
-        const socialSuffixId = (
-          (await prismaClient.title.findUnique({
-            where: {
-              slug: values.socialSuffix.value as string,
-            },
-          })) as Title
-        ).id;
-
-        data = { ...data, socialSuffixId };
-      }
-
-      if (r.values.additionalJobClassification.value) {
-        try {
-          const additionalJobClassificationId = (
-            (await prismaClient.additionalJobClassification.findUnique({
-              where: {
-                slug: r.values.additionalJobClassification.value as string,
-              },
-            })) as AdditionalJobClassification
-          ).id;
-
-          data = { ...data, additionalJobClassificationId };
-        } catch (error) {
-          console.error(
-            "Error - additionalJobClassification slug not found:",
-            r.values.additionalJobClassification.value
-          );
-        }
-      }
-
-      if (r.values.workspace.value) {
-        // TODO: Missing locations in base data?
-        try {
-          const workspaceId = (
-            (await prismaClient.location.findUnique({
-              where: { slug: r.values.workspace.value as string },
-            })) as Location
-          ).id;
-
-          data = { ...data, workspaceId };
-        } catch (error) {
-          console.error(
-            "Error - workspace (location) slug not found:",
-            r.values.workspace.value
-          );
-        }
-      }
 
       if (
         r.values.managerId.value &&
