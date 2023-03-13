f4471077d77d4b9bc9b796c8e9777894f815d7ad
John Mosesman <johnmosesman@gmail.com>
2023-03-13 16:09:06 -0500

    /api/v1/employees should lookup by space -> org -> employees to filter

--- pages/api/v1/employees.ts
+++ pages/api/v1/employees.ts
@@ -1,11 +1,29 @@
 import { prismaClient } from "../../../lib/prisma-client";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { getSpaceForFlatfileSpaceId } from "../../../lib/space";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const spaceId = req.query.spaceId as string;
+
+  const space = await getSpaceForFlatfileSpaceId(spaceId);
+
+  const user = await prismaClient.user.findUnique({
+    where: {
+      id: space.userId,
+    },
+  });
+
+  if (!user) {
+    throw new Error(`No user found for space ${spaceId}`);
+  }
+
   const employees = await prismaClient.employee.findMany({
+    where: {
+      organizationId: user.organizationId,
+    },
     select: { employeeId: true },
   });
 
