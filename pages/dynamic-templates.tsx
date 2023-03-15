3a4806f4cec75089847ef98f1e12cb638e4ad11c
John Mosesman <johnmosesman@gmail.com>
2023-03-15 15:15:12 -0500

    Working ui

--- pages/dynamic-templates.tsx
+++ pages/dynamic-templates.tsx
@@ -34,12 +34,18 @@ interface Props {}
 //   },
 // }),
 
-const initialOptions: { [key: string]: string } = {
-  ft: "Full-Time",
-  pt: "Part-Time",
-  tm: "Temporary",
-  ct: "Contract",
-};
+interface Option {
+  id: number;
+  input: string;
+  output: string;
+}
+
+const initialOptions: Option[] = [
+  { id: 1, input: "ft", output: "Full-Time" },
+  { id: 2, input: "pt", output: "Part-Time" },
+  { id: 3, input: "tm", output: "Temporary" },
+  { id: 4, input: "ct", output: "Contract" },
+];
 
 const DynamicTemplates: NextPageWithLayout<Props> = ({}) => {
   const [options, setOptions] = useState(initialOptions);
@@ -57,49 +63,83 @@ const DynamicTemplates: NextPageWithLayout<Props> = ({}) => {
           <p className="w-1/2">Output value on record</p>
         </div>
 
-        {Object.keys(options).map((key) => {
-          return (
-            <div className="flex flex-row justify-between items-center mb-2">
-              <input
-                type="text"
-                value={key}
-                className="border border-gray-200 rounded px-4 py-2 mr-2 w-1/2"
-              />
-
-              <input
-                type="text"
-                value={options[key]}
-                className="border border-gray-200 rounded px-4 py-2 w-1/2"
-              />
-
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6 text-gray-300 ml-2 cursor-pointer"
-                onClick={() => {
-                  setOptions((prev) => {
-                    const newOptions = { ...prev };
-                    delete newOptions[key];
-                    return newOptions;
-                  });
-                }}
+        {options
+          .sort((a, b) => a.id - b.id)
+          .map((option) => {
+            return (
+              <div
+                key={option.id}
+                className="flex flex-row justify-between items-center mb-2"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
+                <input
+                  type="text"
+                  defaultValue={option.input}
+                  onChange={(e) => {
+                    const filteredOptions = options.filter((o) => {
+                      return o.id !== option.id;
+                    });
+
+                    setOptions([
+                      ...filteredOptions,
+                      { ...option, input: e.target.value },
+                    ]);
+                  }}
+                  className="border border-gray-200 rounded px-4 py-2 mr-2 w-1/2"
                 />
-              </svg>
-            </div>
-          );
-        })}
+
+                <input
+                  type="text"
+                  defaultValue={option.output}
+                  onChange={(e) => {
+                    const filteredOptions = options.filter((o) => {
+                      return o.id !== option.id;
+                    });
+
+                    setOptions([
+                      ...filteredOptions,
+                      { ...option, output: e.target.value },
+                    ]);
+                  }}
+                  className="border border-gray-200 rounded px-4 py-2 w-1/2"
+                />
+
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6 text-gray-300 ml-2 cursor-pointer"
+                  onClick={() => {
+                    const filteredObjects = options.filter((o) => {
+                      return o.id !== option.id;
+                    });
+
+                    console.log("filteredObjects", filteredObjects);
+
+                    setOptions(filteredObjects);
+                  }}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </div>
+            );
+          })}
 
         <div
           onClick={() => {
-            setOptions({...options, { "": "" }});
+            console.log("add");
+            console.log("options", options);
+
+            const maxId = options.reduce((max, option) => {
+              return Math.max(max, option.id);
+            }, 0);
+
+            setOptions([...options, { id: maxId + 1, input: "", output: "" }]);
           }}
           className="flex flex-row items-center justify-start text-gray-400 text-sm cursor-pointer"
         >
