25681c3d10a6d4703654e6c17deb09e35e5af516
Jason Gong <jasongong83@gmail.com>
2023-03-15 08:59:19 -0400

    add orgId to job query on upserting employee. Edit job seed to allow jobId on employee.

--- lib/seeds/main.ts
+++ lib/seeds/main.ts
@@ -19,7 +19,7 @@ import { DateTime } from "luxon";
 import * as fs from "fs";
 import { parse } from "fast-csv";
 import { hashPassword } from "../user";
-import crypto from "crypto";
+import crypto, { randomUUID } from "crypto";
 import { upsertEmployee } from "../employee";
 import { faker } from "@faker-js/faker";
 
@@ -40,8 +40,8 @@ export const main = async () => {
   // await upsertAddresses();
   // await createOtherData();
 
-  // const user = await upsertUser();
-  // await upsertJobs(user.organizationId);
+  const user = await upsertUser();
+  await upsertJobs(user.organizationId);
 
   // await seedNewAccount(user);
 };
@@ -723,38 +723,41 @@ export const upsertEmployees = async (organizationId: string) => {
   await Promise.all(directReports);
 };
 
-// const upsertUser = async () => {
-//   const email = "user@email.com";
+const upsertUser = async () => {
+  const companyName = "Company Name";
 
-//   const orgData = {
-//     email,
-//   };
+  const orgData = {
+    companyName,
+  };
 
-//   const organization = await prismaClient.organization.upsert({
-//     where: {
-//       email,
-//     },
-//     create: orgData,
-//     update: {},
-//   });
+  const organization = await prismaClient.organization.upsert({
+    where: {
+      id: randomUUID(),
+      companyName,
+    },
+    create: orgData,
+    update: {},
+  });
 
-//   const data = {
-//     email: email,
-//     password: await hashPassword("badpassword"),
-//     organization: {
-//       connect: {
-//         id: organization.id,
-//       },
-//     },
-//   };
+  const data = {
+    firstName: "First Name",
+    lastName: "Last Name",
+    email: "email@email.com",
+    password: await hashPassword("badpassword"),
+    organization: {
+      connect: {
+        id: organization.id,
+      },
+    },
+  };
 
-//   const user: User = await prismaClient.user.upsert({
-//     where: {
-//       email: data.email,
-//     },
-//     create: data,
-//     update: {},
-//   });
+  const user: User = await prismaClient.user.upsert({
+    where: {
+      email: data.email,
+    },
+    create: data,
+    update: {},
+  });
 
-//   return user;
-// };
+  return user;
+};
