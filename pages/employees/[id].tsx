76658c24be768ecbee5ef8280a72953e1b045fc7
mhpjay422 <12041898+mhpjay422@users.noreply.github.com>
2023-03-27 12:27:19 -0400

    edit employees ui to match data (#116)

--- pages/employees/[id].tsx
+++ pages/employees/[id].tsx
@@ -1,5 +1,6 @@
 import { GetServerSideProps, NextPage } from "next";
 import { Prisma, PrismaClient } from "@prisma/client";
+import { DateTime } from "luxon";
 
 const employeeWithRelations = Prisma.validator<Prisma.EmployeeArgs>()({
   include: {
@@ -17,40 +18,66 @@ interface Props {
 
 const Employees: NextPage<Props> = ({ employee }) => {
   return (
-    <div className="overflow-hidden bg-white shadow sm:rounded-lg">
-      <div className="px-4 py-5 sm:px-6">
-        <h3 className="text-lg font-medium leading-6 text-gray-900">
-          Employee Information
-        </h3>
-        <p className="mt-1 max-w-2xl text-sm text-gray-500">
-          Employee personal details
-        </p>
-      </div>
-      <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
-        <dl className="sm:divide-y sm:divide-gray-200">
-          <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">Full name</dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-              {employee.firstName} {employee.lastName}
-            </dd>
-          </div>
-          <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">Manager</dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-              {employee.manager && (
-                <div>
-                  {employee.manager.firstName} {employee.manager.lastName}
-                </div>
-              )}
-            </dd>
-          </div>
-          <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">Title</dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-              {employee.positionTitle}
-            </dd>
-          </div>
-        </dl>
+    <div className="px-4 sm:px-6 lg:px-8 w-1/2">
+      <div className="overflow-hidden bg-white shadow sm:rounded-lg">
+        <div className="px-4 py-5 sm:px-6">
+          <h3 className="text-lg font-medium leading-6 text-gray-900">
+            Employee Information
+          </h3>
+          <p className="mt-1 max-w-2xl text-sm text-gray-500">
+            Employee personal details
+          </p>
+        </div>
+        <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
+          <dl className="sm:divide-y sm:divide-gray-200">
+            <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">Full name</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                {employee.firstName} {employee.lastName}
+              </dd>
+            </div>
+            <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">Manager</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                {employee.manager && (
+                  <div>
+                    {employee.manager.firstName} {employee.manager.lastName}
+                  </div>
+                )}
+              </dd>
+            </div>
+            <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">Title</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                {employee.positionTitle}
+              </dd>
+            </div>
+            <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">Hire Date</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                {DateTime.fromJSDate(employee.hireDate).toFormat("MM/dd/yyyy")}
+              </dd>
+            </div>
+            <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">End Date</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                {employee.endEmploymentDate
+                  ? DateTime.fromJSDate(employee.endEmploymentDate).toFormat(
+                      "MM/dd/yyyy"
+                    )
+                  : "-"}
+              </dd>
+            </div>
+            <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">
+                Weekly Hours
+              </dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                {employee.scheduledWeeklyHours}
+              </dd>
+            </div>
+          </dl>
+        </div>
       </div>
     </div>
   );
