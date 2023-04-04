21b8c98715d026a9094a459e50008867016198a3
Jason Gong <jasongong83@gmail.com>
2023-04-04 13:24:05 -0400

    hotfix createSpace PROD

--- pages/api/flatfile/create-space.tsx
+++ pages/api/flatfile/create-space.tsx
@@ -44,6 +44,7 @@ export default async function handler(
   const flatfileSpaceData = await createSpace({
     accessToken,
     spaceConfigId: process.env.ONBOARDING_SPACE_CONFIG_ID as string,
+    environmentId: process.env.FLATFILE_ENVIRONMENT_ID as string,
   });
   const spaceId = flatfileSpaceData.id;
 
