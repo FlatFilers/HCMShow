2bd6ef26b9d34e9fcf8d200bc676eadc2925d44b
John Mosesman <johnmosesman@users.noreply.github.com>
2023-04-05 15:20:25 -0500

    Cleanup and show/hide dynamic space button (#127)

--- pages/dynamic-templates.tsx
+++ pages/dynamic-templates.tsx
@@ -229,13 +229,24 @@ const DynamicTemplates: NextPageWithLayout<Props> = ({
         </div>
       </div>
 
-      <button
-        onClick={() => setShowSpace(true)}
-        className="px-4 py-2 inline-flex items-center justify-center rounded-md border text-sm font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 sm:w-auto mb-6 bg-primary text-white border-transparent"
-      >
-        Generate Space
-        <SparklesIcon className="w-4 h-4 ml-2" />
-      </button>
+      {!showSpace && (
+        <button
+          onClick={() => setShowSpace(true)}
+          className="px-4 py-2 inline-flex items-center justify-center rounded-md border text-sm font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 sm:w-auto mb-6 bg-primary text-white border-transparent"
+        >
+          Generate Space
+          <SparklesIcon className="w-4 h-4 ml-2" />
+        </button>
+      )}
+      {showSpace && (
+        <button
+          onClick={() => setShowSpace(false)}
+          className="px-4 py-2 inline-flex items-center justify-center rounded-md border border-primary text-sm font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 sm:w-auto mb-6 bg-white text-primary "
+        >
+          Close Space
+          <SparklesIcon className="w-4 h-4 ml-2" />
+        </button>
+      )}
 
       {error && <div>{error}</div>}
       {!error && showSpace && (
