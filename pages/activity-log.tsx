aac58f6e12fce3336cda106c158485952101666f
Jason Gong <jasongong83@gmail.com>
2023-04-18 17:32:28 -0400

    remove unneeded code

--- pages/activity-log.tsx
+++ pages/activity-log.tsx
@@ -40,8 +40,6 @@ const ActivityLog: NextPage<Props> = ({ actions }) => {
     }
   };
 
-  const handleConfirm = () => {};
-
   const router = useRouter();
   useEffect(() => {
     if (router.query.flash === "success") {
@@ -56,112 +54,109 @@ const ActivityLog: NextPage<Props> = ({ actions }) => {
     }
   }, []);
   return (
-    <div>
-      <div className="px-4 sm:px-6 lg:px-8">
-        <div className="sm:flex sm:items-center">
-          <div className="w-full flex flex-row justify-between mb-8">
-            <div className="sm:flex-auto">
-              <h1 className="text-xl font-semibold text-gray-900">
-                Activity Log
-              </h1>
-              <p className="mt-2 text-sm text-gray-700">
-                Your activity history from Flatfile will show here.
-              </p>
-            </div>
-            <form
-              ref={formRef}
-              action="/api/v1/reset-account"
-              method="post"
-              onSubmit={handleSubmit}
-            >
-              <button
-                disabled={isSubmitting}
-                type="submit"
-                className={`${
-                  isSubmitting
-                    ? "hover:cursor-not-allowed"
-                    : "hover:bg-red-600 hover:text-white"
-                } bg-white inline-flex items-center justify-center rounded-xl border border-red-600 px-12 py-2 text-base text0 font-medium text-red-600 shadow-sm focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-offset-2 sm:w-auto}`}
-              >
-                {buttonText}
-                <ArrowPathRoundedSquareIcon className="w-5 h-5 ml-2" />
-              </button>
-            </form>
+    <div className="px-4 sm:px-6 lg:px-8">
+      <div className="sm:flex sm:items-center">
+        <div className="w-full flex flex-row justify-between mb-8">
+          <div className="sm:flex-auto">
+            <h1 className="text-xl font-semibold text-gray-900">
+              Activity Log
+            </h1>
+            <p className="mt-2 text-sm text-gray-700">
+              Your activity history from Flatfile will show here.
+            </p>
           </div>
+          <form
+            ref={formRef}
+            action="/api/v1/reset-account"
+            method="post"
+            onSubmit={handleSubmit}
+          >
+            <button
+              disabled={isSubmitting}
+              type="submit"
+              className={`${
+                isSubmitting
+                  ? "hover:cursor-not-allowed"
+                  : "hover:bg-red-600 hover:text-white"
+              } bg-white inline-flex items-center justify-center rounded-xl border border-red-600 px-12 py-2 text-base text0 font-medium text-red-600 shadow-sm focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-offset-2 sm:w-auto}`}
+            >
+              {buttonText}
+              <ArrowPathRoundedSquareIcon className="w-5 h-5 ml-2" />
+            </button>
+          </form>
         </div>
-        <div className="mt-8 flex flex-col">
-          <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-              <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-                <table className="min-w-full divide-y divide-gray-300">
-                  <thead className="bg-gray-50">
-                    <tr>
-                      <th
-                        scope="col"
-                        className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
-                      >
-                        Action
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                      >
-                        User
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                      >
-                        Date
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                      >
-                        Result
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className="divide-y divide-gray-200 bg-white">
-                    {actions &&
-                      actions.length > 0 &&
-                      actions.map((a) => {
-                        return (
-                          <tr key={a.id}>
-                            <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
-                              {mapActionTypeToLabel(a.type)}
-                            </td>
-                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                              {a.user.email}
-                            </td>
-                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                              {DateTime.fromJSDate(a.createdAt).toFormat(
-                                "MM/dd/yy hh:mm:ss a"
-                              )}
-                            </td>
-                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                              {a.type === ActionType.SyncRecords &&
-                                a.description}
-                              {a.type === ActionType.FileFeedEvent &&
-                                (a.metadata as { description: string })
-                                  .description}
-                              {a.type === ActionType.SyncEmbedRecords &&
-                                a.description}
-                            </td>
-                          </tr>
-                        );
-                      })}
+      </div>
+      <div className="mt-8 flex flex-col">
+        <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+              <table className="min-w-full divide-y divide-gray-300">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                    >
+                      Action
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    >
+                      User
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    >
+                      Date
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    >
+                      Result
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-200 bg-white">
+                  {actions &&
+                    actions.length > 0 &&
+                    actions.map((a) => {
+                      return (
+                        <tr key={a.id}>
+                          <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                            {mapActionTypeToLabel(a.type)}
+                          </td>
+                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                            {a.user.email}
+                          </td>
+                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                            {DateTime.fromJSDate(a.createdAt).toFormat(
+                              "MM/dd/yy hh:mm:ss a"
+                            )}
+                          </td>
+                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                            {a.type === ActionType.SyncRecords && a.description}
+                            {a.type === ActionType.FileFeedEvent &&
+                              (a.metadata as { description: string })
+                                .description}
+                            {a.type === ActionType.SyncEmbedRecords &&
+                              a.description}
+                          </td>
+                        </tr>
+                      );
+                    })}
 
-                    {(!actions || actions.length === 0) && (
-                      <tr>
-                        <td className="py-4 pl-4 pr-3 text-sm text-gray-400 sm:pl-6">
-                          No actions taken yet
-                        </td>
-                      </tr>
-                    )}
-                  </tbody>
-                </table>
-              </div>
+                  {(!actions || actions.length === 0) && (
+                    <tr>
+                      <td className="py-4 pl-4 pr-3 text-sm text-gray-400 sm:pl-6">
+                        No actions taken yet
+                      </td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
