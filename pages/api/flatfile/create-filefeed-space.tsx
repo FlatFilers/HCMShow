9561f2088c455398cec3cbecf28bdcb0f43f0b61
John Mosesman <johnmosesman@gmail.com>
2023-06-12 14:10:02 -0500

    wip

--- pages/api/flatfile/create-filefeed-space.tsx
+++ pages/api/flatfile/create-filefeed-space.tsx
@@ -2,17 +2,16 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Prisma, PrismaClient, Space } from "@prisma/client";
 import { getToken } from "next-auth/jwt";
-import {
-  FlatfileSpaceData,
-  addDocumentToSpace,
-  addGuestToSpace,
-  createSpace,
-  getAccessToken,
-  getSpace,
-  postFile,
-} from "../../../lib/flatfile-legacy";
+import { FlatfileSpaceData } from "../../../lib/flatfile-legacy";
 import { SpaceType } from "../../../lib/space";
 import { fetchFileFromDrive } from "../../../lib/google-drive";
+import {
+  WorkflowType,
+  addGuestToSpace,
+  createSpace,
+  postFile,
+} from "../../../lib/flatfile";
+import { prismaClient } from "../../../lib/prisma-client";
 
 export default async function handler(
   req: NextApiRequest,
@@ -31,8 +30,7 @@ export default async function handler(
     };
   }
 
-  const prisma = new PrismaClient();
-  const user = await prisma.user.findUnique({
+  const user = await prismaClient.user.findUnique({
     where: {
       id: token.sub,
     },
@@ -42,89 +40,51 @@ export default async function handler(
     throw new Error("No user found");
   }
 
-  const accessToken = await getAccessToken();
-
+  const workflow = WorkflowType.FileFeed;
   const environmentId = process.env.FILEFEED_ENVIRONMENT_ID as string;
 
-  const flatfileSpaceData = await createSpace({
-    accessToken,
-    spaceConfigId: process.env.FILEFEED_SPACE_CONFIG_ID as string,
-    environmentId,
+  const spaceResult = await createSpace({
+    workflow,
     userId: user.id,
-    focusBgColor: "#616A7D",
-    backgroundColor: "#090B2B",
+    environmentId,
+    spaceName: "Filefeed",
   });
 
-  const addGuestToSpaceResponse = await addGuestToSpace(
-    user,
-    flatfileSpaceData,
-    accessToken,
-    environmentId
-  );
+  if (!spaceResult) {
+    return res.redirect(
+      "/file-feed?flash=error&message=Error setting up Flatfile"
+    );
+  }
 
-  console.log("addGuestToSpaceResponse", addGuestToSpaceResponse);
+  const guestResult = await addGuestToSpace({
+    workflow,
+    environmentId,
+    email: user.email,
+    name: `${user.firstName} ${user.lastName}`,
+    spaceId: spaceResult.id,
+  });
 
-  if (
-    addGuestToSpaceResponse.errors &&
-    addGuestToSpaceResponse.errors[0].message
-  ) {
+  if (!guestResult) {
     res.redirect("/file-feed?flash=error&message=Error setting up Flatfile");
   }
 
-  const flatfileSpaceDataRefetch = await getSpace(
-    flatfileSpaceData.id,
-    accessToken
-  );
-
-  console.log("flatfileSpaceDataRefetch", flatfileSpaceDataRefetch);
-
-  // TODO: Don't need this, already sending an email on create?
-  // const inviteGuestsToSpaceResponse = await inviteGuestToSpace(
-  //   addGuestToSpaceResponse[0].id,
-  //   spaceId,
-  //   accessToken
-  // );
-
-  // console.log('inviteGuestsToSpaceResponse', inviteGuestsToSpaceResponse.success);
-
-  const space: Space = await prisma.space.create({
+  const space: Space = await prismaClient.space.create({
     data: {
       userId: user.id,
       type: SpaceType.FileFeed,
-      flatfileData:
-        flatfileSpaceDataRefetch as unknown as Prisma.InputJsonValue,
+      flatfileData: spaceResult as unknown as Prisma.InputJsonValue,
     },
   });
-
-  const initialDocumentBody = `<div> 
-  <h1 style="margin-bottom: 36px;">Welcome! We're excited to help you import your data to HCM Show.</h1>
-  <h2 style="margin-top: 0px; margin-bottom: 12px;">Follow the steps below to get started:</h2>
-  <h2 style="margin-bottom: 0px;">1. Upload your file</h2>
-  <p style="margin-top: 0px; margin-bottom: 8px;">Click "Files" in the left-hand sidebar, and upload the sample data you want to import into Flatfile. You can do this by clicking "Add files" or dragging and dropping the file onto the page.</p>
-  <h2 style="margin-bottom: 0px;">2. Import the Benefit Elections Data</h2>
-  <p style="margin-top: 0px; margin-bottom: 8px;">Click "Import" and select the benefit elections data. Follow the mapping instructions in Flatfile to complete the import. Once the data has been mapped, it will be loaded into Flatfile's table UI, where validations and transformations have been applied.</p>
-  <h2 style="margin-bottom: 0px;">3. Validate and Transform Data</h2>
-  <p style="margin-top: 0px; margin-bottom: 8px;">Make sure to verify that your data is correctly formatted and transformed by Flatfile. Flatfile will handle formatting dates, rounding amounts, and validating the existence of employees and benefit plans for you! If there are any issues or errors, you can easily address them within Flatfile's user interface.</p>
-  <h2 style="margin-bottom: 0px;">4. Load Data into HCM.Show</h2>
-  <p style="margin-top: 0px; margin-bottom: 12px;">Once the data has been validated and transformed, use the “Push records to HCM.show” button to load data into the HCM.Show application.</p>
-  <h2 style="margin-bottom: 0px;">5. Return to HCM.Show</h2>
-  <p style="margin-top: 0px; margin-bottom: 36px;">Once you have loaded the data from Flatfile to HCM Show, return to HCM.Show and navigate to the Data Templates section within the application to view the benefit elections data that you have just loaded.</p>
-  <h3 style="margin-top: 0px; margin-bottom: 12px;">Remember, if you need any assistance, you can always refer back to this page by clicking "Welcome" in the left-hand sidebar!</h3>
-</div>`;
-
-  const addDocumentToSpaceResponse = await addDocumentToSpace(
-    "Welcome",
-    initialDocumentBody,
-    flatfileSpaceData.id,
-    accessToken
-  );
-
-  // console.log("addDocumentToSpaceResponse", addDocumentToSpaceResponse);
 
   const file = await fetchFileFromDrive();
 
   const spaceId = (space.flatfileData as unknown as FlatfileSpaceData).id;
-  await postFile(accessToken, spaceId, file);
+  await postFile({
+    workflow,
+    environmentId,
+    spaceId,
+    file,
+  });
 
   res.redirect("/file-feed?flash=success&message=Setup Flatfile!");
 }
