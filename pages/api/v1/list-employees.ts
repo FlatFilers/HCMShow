7348ae3126c828df9569574918c71aa9350dbab7
Jason Gong <jasongong83@gmail.com>
2023-07-27 10:54:56 -0400

    add API endpoint to get list of employees

---
+++ pages/api/v1/list-employees.ts
@@ -0,0 +1,32 @@
+import { prismaClient } from "../../../lib/prisma-client";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (typeof req.query.userId !== "string") {
+    res.status(400).send("Invalid or missing userId");
+    return;
+  }
+
+  const userId = req.query.userId;
+
+  const user = await prismaClient.user.findUnique({
+    where: {
+      id: userId,
+    },
+  });
+
+  if (!user) {
+    throw new Error(`No user found for ID# ${userId}`);
+  }
+
+  const employees = await prismaClient.employee.findMany({
+    where: {
+      organizationId: user.organizationId,
+    },
+  });
+
+  res.send(employees);
+}
