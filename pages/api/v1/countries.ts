029c23c939256bc8265d0656a46e3dacf39ea479
John Mosesman <johnmosesman@gmail.com>
2023-08-02 15:12:15 -0500

    undo

--- pages/api/v1/countries.ts
+++ pages/api/v1/countries.ts
@@ -1,19 +1,20 @@
-import { prismaClient } from "../../../lib/prisma-client";
-import type { NextApiRequest, NextApiResponse } from "next";
+import { prismaClient } from '../../../lib/prisma-client';
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const countries = await prismaClient.country.findMany();
+  
+  const countries = await prismaClient.country.findMany();  
 
-  const listCountries = countries.map((country) => {
+  const listCountries = countries.map(country => {
     return {
       name: country.name,
       type: country.type,
       code: country.code,
-    };
-  });
+    }
+  })
 
-  res.send({ listCountries });
+  res.send({listCountries})
 }
