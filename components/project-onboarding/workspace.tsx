abbf5bf36d20ea5a8d9d7e910d2b57481947f4c2
John Mosesman <johnmosesman@gmail.com>
2023-10-04 15:05:27 -0500

    project onboarding skin

--- components/project-onboarding/workspace.tsx
+++ components/project-onboarding/workspace.tsx
@@ -31,38 +31,38 @@ type Props = {
 
 const Workspace = ({ fileName, flatfileSpaceId }: Props) => {
   return (
-    <>
+    <div className="text-white">
       <p className="text-2xl mb-8">Your Flatfile space is configured. 🎉</p>
 
       <div className="flex flex-row justify-between">
         <div>
           <p className="font-semibold mb-4">Upload Records in Flatfile</p>
-          <p className="text-gray-600 mb-2 max-w-lg">
+          <p className="text-gray-400 mb-2 max-w-lg">
             Click the "Visit Flatfile Space" button below to access your
             dedicated space in Flatfile and receive a sign-in link via email.
           </p>
-          <p className="text-gray-600 mb-2 max-w-lg">
+          <p className="text-gray-400 mb-2 max-w-lg">
             Upload the previously downloaded sample data after accessing the
             space.
           </p>
-          <p className="text-gray-600 mb-4 max-w-lg">
+          <p className="text-gray-400 mb-4 max-w-lg">
             Once the data is uploaded and loaded into HCM Show, return to this
             page to review the data within the application.
           </p>
 
           <div className="flex flex-row items-center mb-8">
             <GoToSpace workflow="onboarding" flatfileSpaceId={flatfileSpaceId}>
-              <button className="inline-flex flex-row items-center justify-between rounded-md border border-transparent bg-project-onboarding px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-project-onboarding focus:outline-none focus:ring-2 focus:ring-project-onboarding focus:ring-offset-2 mr-4">
+              <button className="button-bg inline-flex flex-row items-center justify-between rounded-md border border-transparent px-4 py-2 text-sm font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 mr-4">
                 Visit Flatfile Space
                 <ArrowTopRightOnSquareIcon className="w-4 h-4 ml-1" />
               </button>
             </GoToSpace>
           </div>
 
-          <p className="text-xs block text-gray-600">
+          <p className="text-xs block text-gray-400">
             To download the sample data again{" "}
             <a
-              className="underline text-project-onboarding"
+              className="underline text-gray-400"
               download={fileName}
               href={fileName}
             >
@@ -77,7 +77,7 @@ const Workspace = ({ fileName, flatfileSpaceId }: Props) => {
           features={features}
         />
       </div>
-    </>
+    </div>
   );
 };
 
--- components/shared/step-list.tsx
+++ components/shared/step-list.tsx
@@ -8,28 +8,6 @@ export interface Step {
 type Props = { type: string; steps: Step[] };
 
 const StepList = ({ type, steps }: Props) => {
-  let iconClasses = "";
-  let textClasses = "";
-  let circleClasses = "";
-
-  if (type === "project-onboarding") {
-    iconClasses = "bg-project-onboarding";
-    textClasses = "text-project-onboarding";
-    circleClasses = "bg-project-onboarding-highlight";
-  } else if (type === "embedded-portal") {
-    iconClasses = "bg-embedded-portal";
-    textClasses = "text-embedded-portal";
-    circleClasses = "bg-embedded-portal-highlight";
-  } else if (type === "file-feed") {
-    iconClasses = "bg-file-feed";
-    textClasses = "text-file-feed";
-    circleClasses = "bg-file-feed-highlight";
-  } else if (type === "dynamic-portal") {
-    iconClasses = "bg-dynamic-portal";
-    textClasses = "text-dynamic-portal";
-    circleClasses = "bg-dynamic-portal-highlight";
-  }
-
   return (
     <div className="">
       <nav className="flex justify-center" aria-label="Progress">
@@ -41,7 +19,7 @@ const StepList = ({ type, steps }: Props) => {
                   <span className="flex items-start">
                     <span className="relative flex h-5 w-5 flex-shrink-0 items-center justify-center">
                       <CheckCircleIcon
-                        className={`${textClasses} h-full w-full`}
+                        className={`text-gray-400 h-full w-full`}
                         aria-hidden="true"
                       />
                     </span>
@@ -57,13 +35,13 @@ const StepList = ({ type, steps }: Props) => {
                     aria-hidden="true"
                   >
                     <span
-                      className={`${circleClasses} opacity-30 absolute h-4 w-4 rounded-full`}
+                      className={`bg-white opacity-30 absolute h-4 w-4 rounded-full`}
                     />
                     <span
-                      className={`${iconClasses} relative block h-2 w-2 rounded-full`}
+                      className={`bg-white relative block h-2 w-2 rounded-full`}
                     />
                   </span>
-                  <span className={`${textClasses} ml-3 text-sm font-medium`}>
+                  <span className={`text-white ml-3 text-sm font-medium`}>
                     {step.name}
                   </span>
                 </a>
@@ -74,7 +52,7 @@ const StepList = ({ type, steps }: Props) => {
                       className="relative flex h-5 w-5 flex-shrink-0 items-center justify-center"
                       aria-hidden="true"
                     >
-                      <div className="h-2 w-2 rounded-full bg-gray-300" />
+                      <div className="h-2 w-2 rounded-full bg-gray-500" />
                     </div>
                     <p className="ml-3 text-sm font-medium text-gray-500">
                       {step.name}
