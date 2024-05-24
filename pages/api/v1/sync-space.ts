93f6a01bd53efa08b547760687956e4d6f588f6a
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2024-05-24 15:07:07 -0600

    Change v1 back to successfullySyncedFlatfileRecordIds

--- pages/api/v1/sync-space.ts
+++ pages/api/v1/sync-space.ts
@@ -124,7 +124,7 @@ export default async function handler(
 
     res.status(200).json({
       success: true,
-      syncedFlatfileRecordIds,
+      successfullySyncedFlatfileRecordIds: syncedFlatfileRecordIds,
     });
     return;
   } else if (space.type === SpaceType.Embed) {
