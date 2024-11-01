a393d00a9d5161b3dee81ad2e9470586d9a7bb62
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2024-11-01 14:00:28 -0600

    Default language

--- components/language-context.tsx
+++ components/language-context.tsx
@@ -38,8 +38,10 @@ export const LanguageProvider: React.FC<LanguageProviderProps> = ({
   useEffect(() => {
     const getStoredLanguage = () => {
       if (typeof window !== "undefined") {
-        const savedLanguage = localStorage.getItem("language");
-        return (savedLanguage as SupportedLanguage) || "en";
+        const storedLanguage = localStorage.getItem(
+          "language"
+        ) as SupportedLanguage;
+        return storedLanguage || "en";
       }
       return "en";
     };
